a85ae88ce7bfe3d3ecddb3f1a189a26cd37f3d67
anfisa-source <ashi0010@student.monash.edu>
2019-09-03 13:02:02 +1000

    added mongoDB

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ let express = require('express');
 let app = express();
 let bodyParser=require("body-parser");
 let ejs =require("ejs");
-db = new Array();
+
+//db = new Array();
+let mongodb = require('mongodb');
+let mongoDBClient = mongodb.MongoClient;
+
+let db = null;
+let col = null;
+let url = "mongodb://localhost:27017";
+mongoDBClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (err, client) {
+
+    db = client.db("week6");
+    col = db.collection("tasks");
+    console.log("Connected");
+
+});
 
 
 app.use(express.static('img'));
@@ -27,12 +44,37 @@ app.get("/addTask", (req,res)=>{
 
 app.post("/addNewTask", (req,res) => {
     console.log(req.body)
-    db.push(req.body)
+    let newtask = req.body;
+    col.insertOne({ name: newtask.name, assign: newtask.assign, due: newtask.due, status: newtask.status, desc: newtask.desc });
+    res.redirect('/listTasks');
     //res.send("Task submitted")
 });
 
 app.get("/listTasks", (req,res)=>{
-    res.render(showView + "listAllTasks.html", data = db)
+    col.find({}).toArray(function (err, data) {
+        res.render(showView +'listAllTasks', { col: data });
+    });
+});
+
+
+
+app.get('/deletetask', function (req, res) {
+    res.sendFile(showView + 'deletetask.html');
+});
+
+app.post('/deletetaskdata', function (req, res) {
+    let task2del = req.body;
+    console.log(task2del);
+    let filter = { "_id" : task2del._id };
+    col.deleteOne(filter);
+    res.redirect('/listTasks');
+});
+
+
+app.get('/deleteall', function (req, res) {
+    col.deleteMany({});
+    res.redirect('/listTasks');
+
 });
 
 app.listen(8080);
